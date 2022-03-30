b59d52b2263171771064e9bd8f79fc4db7fbfadd
Carlos <chenrique452@gmail.com>
2022-03-30 13:07:03 -0300

    Observers adicionados

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,57 @@ document.querySelectorAll('.item').forEach( element => {
         Menu.style.width = '0px'
     })
 })
+
+
+const firstProject = document.querySelector('.frontend-mentor-projects')
+const softwareProject = document.querySelector('.projects-session')
+const hero = document.querySelector('.hero')
+
+
+
+const options ={
+    rootMargin: "-150px"
+}
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach( entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('slide-right')
+        }
+
+        else {
+            entry.target.classList.remove('slide-right')
+        }
+    })
+}, options)
+
+
+const softwareProjectObserver = new IntersectionObserver(function(entries, observer){
+    entries.forEach( entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('fade-in')
+        }
+
+        else {
+            entry.target.classList.remove('fade-in')
+        }
+    })
+}, options)
+
+const heroObserver = new IntersectionObserver(function(entries, observer){
+    entries.forEach( entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('fade-in')
+        }
+
+        else {
+            entry.target.classList.remove('fade-in')
+        }
+    })
+})
+
+
+
+observer.observe(firstProject)
+softwareProject.observe(softwareProject)
+heroObserver.observe(hero)
